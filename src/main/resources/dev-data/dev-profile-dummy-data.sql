4420675c0827d6bb7f4c0888f82e25e56523b455
Muhamad Gafar <muhamad_gafar@hotmail.com>
2021-04-22 22:22:31 +0100

    Added solutions

--- src/main/resources/dev-data/dev-profile-dummy-data.sql
+++ src/main/resources/dev-data/dev-profile-dummy-data.sql
@@ -4,8 +4,8 @@ values ('FX');
 insert into department(name)
 values ('Credit');
 
-insert into tickets(author, description, time_created, time_updated, title, department_id)
-values ('Muhamad Gafar', 'The VDI has decided to stop working again', '2021-04-13 11:02:02', '2021-04-13 11:02:03', 'VDI Issues', 1L);
+insert into tickets(author, description, time_created, time_updated, title, department_id, status, solution)
+values ('Muhamad Gafar', 'The VDI has decided to stop working again', '2021-04-13 11:02:02', '2021-04-13 11:02:03', 'VDI Issues', 1L, 'DONE', 'Restart the VDI');
 
-insert into tickets(author, description, time_created, time_updated, title, department_id)
-values ('Not Muhamad Gafar', 'My Symphony has stopped receiving messages', '2021-04-13 11:02:02', '2021-04-13 11:02:03', 'Symphony problems', 2L);
+insert into tickets(author, description, time_created, time_updated, title, department_id, status)
+values ('Not Muhamad Gafar', 'My Symphony has stopped receiving messages', '2021-04-13 11:02:02', '2021-04-13 11:02:03', 'Symphony problems', 2L, 'OPEN');
